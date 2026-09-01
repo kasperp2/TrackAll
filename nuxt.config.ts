b3abc13b5ef75824a44a69e8bb9046da45b74617
Kasper <kasperpborum@gmail.com>
2026-09-01 17:17:55 +0200

    NuxtHub init

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -2,6 +2,9 @@
 export default defineNuxtConfig({
   compatibilityDate: '2025-07-15',
   devtools: { enabled: true },
-  modules: ['@nuxt/ui'],
-  css: ['~/assets/css/main.css']
+  modules: ['@nuxt/ui', '@nuxthub/core'],
+  css: ['~/assets/css/main.css'],
+  hub: {
+    db: 'postgresql'
+  }
 })
